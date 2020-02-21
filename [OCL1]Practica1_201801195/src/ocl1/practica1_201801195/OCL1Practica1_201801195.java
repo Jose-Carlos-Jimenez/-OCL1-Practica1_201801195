a0af8c6d52477bf62d8fe8d92b4a4805368b4847
Jose Carlos Jimenez <55406693+Jose-Carlos-Jimenez@users.noreply.github.com>
2020-02-20 21:34:33 -0600

    Inicialización del proyecto.

---
+++ [OCL1]Practica1_201801195/src/ocl1/practica1_201801195/OCL1Practica1_201801195.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ocl1.practica1_201801195;
+
+/**
+ *
+ * @author jose5
+ */
+public class OCL1Practica1_201801195 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
